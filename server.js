f1bdcd2d24427e22b08d1b0260bbb4c9ecc81a84
venusofstar <sumidomikeangelo24@gmail.com>
2025-12-10 14:09:26 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+// Home page
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>AuthInfo Proxy</title>
+      </head>
+      <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
+        <h1>WELCOME</h1>
+        <p>😀</p>
+        <p>ENJOY YOUR LIFE</p>
+      </body>
+    </html>
+  `);
+});
+
+// Proxy route: /:channelId/manifest.mpd
+app.get("/:channelId/manifest.mpd", (req, res) => {
+  const { channelId } = req.params;
+
+  // URL without AuthInfo
+  const goToURL = `http://143.44.136.67:6060/001/2/ch0000009099000000${channelId}/manifest.mpd?JITPDRMType=Widevine&JITPMediaType=DASH&virtualDomain=001.live_hls.zte.com&ztecid=ch00000090990000001093&m4s_min=1&usersessionid=189096448&IASHttpSessionId=RR20452520251204070100095540&NeedJITP=1`;
+
+  res.redirect(goToURL);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
