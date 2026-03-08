b465f9f3fdd63d950d61ed38e1e7625d3eb8081e
venusofstar <sumidomikeangelo24@gmail.com>
2026-03-08 15:52:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const { spawn } = require("child_process");
+
 const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (req,res)=>{
-res.send("HLS Restream Running");
-});
+const ffmpeg = spawn("bash", ["start.sh"]);
+
+ffmpeg.stdout.on("data", data => console.log(data.toString()));
+ffmpeg.stderr.on("data", data => console.log(data.toString()));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log("Server running"));
+
+app.listen(PORT, () => {
+console.log("Server running on port " + PORT);
+});
