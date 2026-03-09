0dbb9627c42cd5d1033d1cc402ac2e7464e25310
venusofstar <sumidomikeangelo24@gmail.com>
2026-03-09 15:13:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 mkdir -p public
 
-VIDEO="https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/exchange300024xokvd_300024_3000/chunklist_video.m3u8?hdntl=exp=1773041611~acl=%2f*~id=6f33ed7d-29b0-4c66-b66b-20ca084afd3c~data=hdntl,dWlkPVc5dVJhanxiYjFlZDQ2Ny1hODRhLTQ1Y2EtOGMyNS0wNGI1NmU1NGYzZWEmaXA9MTEyLjIwMS45Ni4xMjYmZXhwPTE3NzMwNDE2NDAmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=9d586215be83237dc3796ee4e4d8376e1813c10b24f59f085dcc8c754e87f7a0"
-AUDIO="https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/exchange300024xokvd_300024_4500/chunklist_audio.m3u8?hdntl=exp=1773041611~acl=%2f*~id=6f33ed7d-29b0-4c66-b66b-20ca084afd3c~data=hdntl,dWlkPVc5dVJhanxiYjFlZDQ2Ny1hODRhLTQ1Y2EtOGMyNS0wNGI1NmU1NGYzZWEmaXA9MTEyLjIwMS45Ni4xMjYmZXhwPTE3NzMwNDE2NDAmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=9d586215be83237dc3796ee4e4d8376e1813c10b24f59f085dcc8c754e87f7a0"
+VIDEO="https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/exchange300024xokvd_300024_3000/chunklist_video.m3u8?hdntl=exp=1773126632~acl=%2f*~id=4a205fdf-7f33-49c2-9451-44d17b805b6a~data=hdntl,dWlkPWNGaDZGenw2YzcyNmIzMS03NDAxLTRkMDgtYWM1Zi1jNDFiOWFiYzU0NWUmaXA9MTgwLjE5MC4xNzIuNDEmZXhwPTE3NzMxMjY2NjAmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=a903af24095cd86337c5113d0ad0691d0aca55178bdb2022314e61420e32b351"
+AUDIO="https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/exchange300024xokvd_300024_4500/chunklist_audio.m3u8?hdntl=exp=1773126632~acl=%2f*~id=4a205fdf-7f33-49c2-9451-44d17b805b6a~data=hdntl,dWlkPWNGaDZGenw2YzcyNmIzMS03NDAxLTRkMDgtYWM1Zi1jNDFiOWFiYzU0NWUmaXA9MTgwLjE5MC4xNzIuNDEmZXhwPTE3NzMxMjY2NjAmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=a903af24095cd86337c5113d0ad0691d0aca55178bdb2022314e61420e32b351"
 
 ffmpeg \
 -user_agent "Mozilla/5.0" \
