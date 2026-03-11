c4693443ebe6b3b8b9eefa2de847f1951f797bdf
venusofstar <sumidomikeangelo24@gmail.com>
2026-03-11 16:51:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,17 +8,24 @@ AUDIO="https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/exchang
 
 ffmpeg \
 -user_agent "Mozilla/5.0" \
+-loglevel error \
 -reconnect 1 \
 -reconnect_streamed 1 \
 -reconnect_delay_max 5 \
+-fflags +genpts \
+-thread_queue_size 2048 \
 -i "$VIDEO" \
+-thread_queue_size 2048 \
 -i "$AUDIO" \
 -map 0:v:0 \
 -map 1:a:0 \
 -c:v copy \
--c:a copy \
+-c:a aac \
+-b:a 128k \
+-ar 48000 \
+-af aresample=async=1 \
 -f hls \
--hls_time 4 \
--hls_list_size 6 \
--hls_flags delete_segments \
+-hls_time 3 \
+-hls_list_size 8 \
+-hls_flags delete_segments+independent_segments \
 public/stream.m3u8
