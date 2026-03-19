6dbdcbf362cc0c0c08fe22a8e5cb430497cfc9bd
venusofstar <sumidomikeangelo24@gmail.com>
2026-03-19 20:13:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 // 🔥 Your streams
-const VIDEO = "https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/exchange300024xokvd_300024_3000/chunklist_video.m3u8?hdntl=exp=1774007657~acl=%2f*~id=b978e039-09bf-4181-b4fe-beb08bdbe797~data=hdntl,dWlkPWNGaDZGenw2YzcyNmIzMS03NDAxLTRkMDgtYWM1Zi1jNDFiOWFiYzU0NWUmaXA9MTEyLjIwMS45Ni4xMjYmZXhwPTE3NzQwMDc2ODUmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=573e2829fe4015c7c8e021c3d349d2998d16abec853093994a18e503e7303202";
+const VIDEO = "https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/exchange300024xokvd_300024_3000/chunklist_video.m3u8?hdntl=exp=1774008569~acl=/*~id=214afb48-ac38-4446-8270-1ccd17b0be4f~data=hdntl,dWlkPW5VTkxWZnw3ZjAxZWMxNS1hMTQwLTRlZDMtODE2My1jOGQ1ZjMwZjg0Y2YmaXA9MTEyLjIwMS45Ni4xMjYmZXhwPTE3NzQwMDg1OTcmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=9ffaf214f1f4bfd7ded21cd6b10760ff18ac8d2fb36925abd71c272873868f67";
 
-const AUDIO = "https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/exchange300024xokvd_300024_4500/chunklist_audio.m3u8?hdntl=exp=1774007657~acl=%2f*~id=b978e039-09bf-4181-b4fe-beb08bdbe797~data=hdntl,dWlkPWNGaDZGenw2YzcyNmIzMS03NDAxLTRkMDgtYWM1Zi1jNDFiOWFiYzU0NWUmaXA9MTEyLjIwMS45Ni4xMjYmZXhwPTE3NzQwMDc2ODUmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=573e2829fe4015c7c8e021c3d349d2998d16abec853093994a18e503e7303202";
+const AUDIO = "https://dice-live-ap.akamaized.net/hls/live/2001903/300024-317970/exchange300024xokvd_300024_4500/chunklist_audio.m3u8?hdntl=exp=1774008569~acl=/*~id=214afb48-ac38-4446-8270-1ccd17b0be4f~data=hdntl,dWlkPW5VTkxWZnw3ZjAxZWMxNS1hMTQwLTRlZDMtODE2My1jOGQ1ZjMwZjg0Y2YmaXA9MTEyLjIwMS45Ni4xMjYmZXhwPTE3NzQwMDg1OTcmZWlkPTMwMDAyNCZjaWQ9ZGNlLnRhcGdvJm9pZD0zMjUmdHlwZT1MSVZF~hmac=9ffaf214f1f4bfd7ded21cd6b10760ff18ac8d2fb36925abd71c272873868f67";
 
 
 
@@ -19,9 +19,9 @@ if (!fs.existsSync(OUTPUT)) fs.mkdirSync(OUTPUT);
 // 🔥 Start FFmpeg restream
 function startStream() {
   const args = [
-    "-headers", "User-Agent: Mozilla/5.0\r\nReferer: https://mydramawave.com/\r\n",
+    "-headers", "User-Agent: Mozilla/5.0\r\nReferer: https://app.blasttv.ph/\r\n",
     "-i", VIDEO,
-    "-headers", "User-Agent: Mozilla/5.0\r\nReferer: https://mydramawave.com/\r\n",
+    "-headers", "User-Agent: Mozilla/5.0\r\nReferer: https://app.blasttv.ph/\r\n",
     "-i", AUDIO,
     "-map", "0:v:0",
     "-map", "1:a:0",
@@ -57,9 +57,9 @@ app.get("/download", (req, res) => {
   const file = "output.mp4";
 
   const ffmpeg = spawn("ffmpeg", [
-    "-headers", "User-Agent: Mozilla/5.0\r\nReferer: https://mydramawave.com/\r\n",
+    "-headers", "User-Agent: Mozilla/5.0\r\nReferer: https://app.blasttv.ph/\r\n",
     "-i", VIDEO,
-    "-headers", "User-Agent: Mozilla/5.0\r\nReferer: https://mydramawave.com/\r\n",
+    "-headers", "User-Agent: Mozilla/5.0\r\nReferer: https://app.blasttv.ph/\r\n",
     "-i", AUDIO,
     "-map", "0:v:0",
     "-map", "1:a:0",
