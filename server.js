1512d41c841f725a684a7b672d2b39bedad5975c
venusofstar <sumidomikeangelo24@gmail.com>
2026-04-10 20:20:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const EPISODES = {
     audio: "https://video-v6.mydramawave.com/vt/560d2369-cbb9-4d2a-bd0d-315391b5323e/tl-PH-d5043ad9-7190-4b23-b9c7-5e8d46a4e678/tl-PH-d307e6e0-87e9-4413-863e-9da4b0d487f1.m3u8"
   },
   ep3: {
-    video: "https://video-v81.mydramawave.com/vt/49efe663-0158-498a-953f-199c7dc6baf3/5_254e6203-4fda-4c10-905c-0de3371d0d4d_transcode_1309546_adaptiveDynamicStreaming_1529555_4.m3u8",
-    audio: "https://video-v81.mydramawave.com/vt/49efe663-0158-498a-953f-199c7dc6baf3/tl-PH-863eb709-b9ef-43b7-93e6-94d4514a175e/tl-PH-96acd809-4121-4e43-873b-78f6708bb659.m3u8"
+    video: "https://video-v81.mydramawave.com/vt/ae45f998-6367-4ea4-8941-df7982c799df/3_cb4ff659-3673-4c7d-b638-a066b9af6f41_transcode_1309546_adaptiveDynamicStreaming_1529555_4.m3u8",
+    audio: "https://video-v81.mydramawave.com/vt/ae45f998-6367-4ea4-8941-df7982c799df/tl-PH-3e9efe94-b028-444a-9552-461e697e907d/tl-PH-105c4380-e595-47c4-9634-98d770800ac6.m3u8"
   },
   ep4: {
-    video: "https://video-v6.mydramawave.com/vt/11c1e24c-ba69-45d7-8fec-d868a021263d/6_9c9b1601-0390-4019-a3ec-7106be4852fa_transcode_1309546_adaptiveDynamicStreaming_1529555_4.m3u8",
-    audio: "https://video-v6.mydramawave.com/vt/11c1e24c-ba69-45d7-8fec-d868a021263d/tl-PH-71cc7843-8a04-4be8-86ff-3480dc732d40/tl-PH-fdf36565-801c-437d-a277-ab2476624690.m3u8"
+    video: "https://video-v6.mydramawave.com/vt/e39cb817-e743-4743-8da5-c9e67c399ac9/4_6f574aa9-88df-4093-9ac9-cd5c22bc131d_transcode_1309546_adaptiveDynamicStreaming_1529555_4.m3u8",
+    audio: "https://video-v6.mydramawave.com/vt/e39cb817-e743-4743-8da5-c9e67c399ac9/tl-PH-36641927-8867-4ba9-9813-eebb606a5d6f/tl-PH-aa5b8a02-2d24-4722-8755-c0df8db1743e.m3u8"
   },
   ep5: {
     video: "https://video-v6.mydramawave.com/vt/1a9514ac-48f1-4f72-9e7b-567736833131/7_b3402287-e086-4f5e-9dd2-a7daf5aecfcf_transcode_1309546_adaptiveDynamicStreaming_1529555_4.m3u8",
